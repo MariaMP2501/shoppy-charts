e56894148a012852d07ef88bbc899ba6e16542e7
MariaMP2501 <alemendez2501@gmail.com>
2022-08-24 22:59:33 +0200

    shoppy charts

---
+++ src/components/Button.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { useStateContext } from '../contexts/ContextProvider';
+
+const Button = ({ icon, bgColor, color, bgHoverColor, size, text, borderRadius }) => {
+  return (
+    <button
+    type="button"
+    style={{backgroundColor: bgColor,color, borderRadius}}
+    className={`text-${size} p-3 hover:drop-shadow-xl`}>
+      {text}
+    </button>
+  )
+}
+
+export default Button
